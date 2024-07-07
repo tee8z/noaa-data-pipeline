f774b0fa63cde683667e0c1f77bac8c7accf91ea
tee8z <tee8z@protonmail.com>
2024-07-06 22:47:57 -0400

    fixing release download script (#22)

--- download_release.sh
+++ download_release.sh
@@ -3,14 +3,17 @@
 # Define variables
 owner="tee8z"
 repo="noaa-data-pipeline"
-tag="v0.0.1"
-asset_name="app-x86_64-unknown-linux-gnu.tar.gz"
+tag="v0.3.0"
+daemon="daemon-x86_64-unknown-linux-gnu.tar.xz"
+file_service="parquet_file_service-x86_64-unknown-linux-gnu.tar.xz"
 
 # Construct URL
-url="https://github.com/$owner/$repo/releases/download/$tag/$asset_name"
+daemon_url="https://github.com/$owner/$repo/releases/download/$tag/$daemon"
+parquet_file_service_url="https://github.com/$owner/$repo/releases/download/$tag/$file_service"
 
-# Download the binary
-curl -LJO $url
+# Download the binaries
+curl -LJO $daemon_url
+curl -LJO $parquet_file_service_url
 
 # Create a directory with the repository name
 mkdir "release-$tag"
@@ -18,6 +21,8 @@ mkdir "release-$tag"
 full_path=$(pwd)/"release-$tag"
 
 # Unzip the downloaded file into the temporary directory
-tar -xzf $asset_name -C $full_path
+tar -xf $daemon -C $full_path
+tar -xf $file_service -C $full_path
 
-rm $asset_name
+rm $daemon
+rm $file_service
